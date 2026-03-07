207b0c8b9ccacde680ea4571dae645812ec18823
Brayan359 <Brayan.gonzalez-mo@uniminuto.edu.co>
2026-03-06 21:15:10 -0500

    Create puntos.py

---
+++ puntos.py
@@ -0,0 +1,14 @@
+#algoritmo
+# 1. pedir el primer puntaje
+# 2. pedir el segundo puntaje
+# comparar puntaje 1 con puntaje 2
+
+#pseudocodigo
+#inicio
+#leer puntaje 1
+#leer puntaje 2
+#escribir si puntaje 1 > segundo puntaje 2
+
+Nacional = int(input("ingrese el primer puntaje"))
+Millos = int(input("ingrese el segundo puntaje"))
+print(Nacional > Millos)
