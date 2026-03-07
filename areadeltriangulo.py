0a1e9059e87fe3c85da3a22cfeaa7a605215ee36
Brayan359 <Brayan.gonzalez-mo@uniminuto.edu.co>
2026-03-06 21:40:10 -0500

    Create areadeltriangulo.py

---
+++ areadeltriangulo.py
@@ -0,0 +1,15 @@
+#solicitar valor altura metros}
+#comparar el resultado con el numero 100
+#calcular formula
+
+#inicio
+#leer base
+#leer altura
+#area= (base*altura)/2
+#escribir area
+#escribir si area > 100
+base=float(input("escribir la base del triangulo"))
+altura=float(input("escribir la altura del triangulo"))
+area=(base*altura)/2
+print("el area del triangulo es:",area)
+print=("el area es mayor que 100",area>100)
