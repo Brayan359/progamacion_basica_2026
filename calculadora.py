aed09c8ab1b40640a0f4d38da3e6f2f8ef565f1d
Brayan359 <Brayan.gonzalez-mo@uniminuto.edu.co>
2026-03-13 18:04:56 -0500

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,33 @@
+#Algoritmo
+#Pedir numero 1
+#Pedir numero 2
+#calcular suma
+#calcular resta
+#calcular multiplicacion
+#calcular division
+#calcular division entera
+#calcular residuo
+#calcular potencia
+#comparar la suma si es mayor a 100
+
+#pseudocodigo 
+#inicio
+#Leer primer numero
+#Leer segundo numero
+#suma=(num1+num2)
+#resta=(num1-num2)
+#multip=(num1*num2)
+#division=(num1/num2)
+#entero=(num1//num2)
+#modulo=(num1%num2)
+#escribir=(si suma es > 100)
+
+num1=int(input("digite el primer numero"))
+num2=int(input("Digite el segundo numero"))
+print("El resultado de de la suma es:",num1+num2)
+print("El resultado de la resta es:",num1-num2)
+print("El resultado de la aplicacion es:",num1*num2)
+print("El resultado de la division es:",num1/num2)
+print("EL resultado de la divi resultado entero:",num1//num2)
+print("El resultado del modulo es:",num1%num2)
+print("La suma es mayor a 100:",num1+num2>100)
