055c54ef2f062d599960b3964f3feda9148e0330
Brayan359 <Brayan.gonzalez-mo@uniminuto.edu.co>
2026-03-13 21:36:29 -0500

    Create Semaforo.py

---
+++ SEMANA_5/Semaforo.py
@@ -0,0 +1,9 @@
+#programa que evalua mas de una condicional
+
+semaforo=input("Dame el color del semaforo")
+if semaforo=="verde":
+    print("puedes seguir")
+elif semaforo=="rojo":
+    print("detente")
+elif semaforo=="amarillo":
+    print("alerta")
