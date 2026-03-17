5db5b81e3b8635488b6c0c9b8b47ecf72865a951
Brayan359 <Brayan.gonzalez-mo@uniminuto.edu.co>
2026-03-17 09:17:22 -0500

    Update Positivo y negativo.py

--- SEMANA_5/Positivo y negativo.py
+++ SEMANA_5/Positivo y negativo.py
@@ -1,5 +1,24 @@
-numero=int(print("Dame un numero"))
-if numero>=0:
-    print("es positico")
+#Algoritmo
+#Digitar numero entero
+#Si el  numero es mayor o igual a 0 es positivo.
+#Si no es mayor o igual que 0 es negativo
+#Si es igual 0 es neutro
+
+#Pseudocodigo
+#Inicio
+#Leer numero
+#Resultado >0
+#Escribir "es positivo"
+#Resultado <0
+#Escribir "es igual 0"
+#Escribir "si es igual 0 es neutro"
+
+
+numero=int(input("digite un numero entero"))
+if numero>0:
+    print("es positivo")
+elif numero<0:
+    print(" es igual a 0")
+
 else:
-    print("es negativo")
+    print("es neutro")
