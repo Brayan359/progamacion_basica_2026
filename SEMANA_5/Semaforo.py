c0169037b82b568a3e2eeaee6e263413d67c6509
Brayan359 <Brayan.gonzalez-mo@uniminuto.edu.co>
2026-03-18 08:45:03 -0500

    Update Semaforo.py

--- SEMANA_5/Semaforo.py
+++ SEMANA_5/Semaforo.py
@@ -1,4 +1,19 @@
-#programa que evalua mas de una condicional
+#Algoritmo
+#Pedir color de semaforo 
+#Si es verde puede seguir
+#Si es rojo detente
+#Si es amarillo es alerta
+
+#Pseudocodigo
+#Leer color de semaforo
+#Resultado semaforo==verde
+#Escribir "puedes seguir"
+#Resultado semaforo==rojo
+#Escribir "detente"
+#Resultado semaforo== amarillo
+#Escribir "alerta"
+#Fin
+
 
 semaforo=input("Dame el color del semaforo")
 if semaforo=="verde":
